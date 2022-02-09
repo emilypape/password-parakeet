939ea6441d06076380cc28f89cc7e3aff8ca459b
emily <eap6787@gmail.com>
2022-02-08 17:28:00 -0700

    starter-code

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,46 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// get password data from user
+function getPasswordData() {
+  var passwordData = {
+    length: 0,
+  };
+
+  // Prompts for password length until user gives a valid number
+  while(true) {
+    var length = prompt('Enter a password length between 8 and 128');
+    if (length >= 8 && length <= 128) {
+      passwordData.length = length;
+      break;
+    }
+    else {
+      prompt('Please pick a valid length')
+    }
+  }
+
+
+
+  return passwordData;
+};
+
+// generates password
+function generatePassword() {
+  var passwordObject = getPasswordData();
+
+  var password = null; // use object to make password that fits criteria
+  
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
