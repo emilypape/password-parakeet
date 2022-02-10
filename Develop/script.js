9048f94408bd7c7e36f53e3c1a297cae40639055
emily <eap6787@gmail.com>
2022-02-09 18:46:54 -0700

    finish generator

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,13 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 // get password data from user
 function getPasswordData() {
   var passwordData = {
@@ -63,34 +70,68 @@ function getPasswordData() {
       alert('Please enter a valid response!');
     }
   }
-  
-
-  
-
-
-
-
   console.log(passwordData);
   return passwordData;
 };
 
 // generates password
 function generatePassword() {
+  // use object to make password that fits criteria
   var passwordObject = getPasswordData();
+  var password = '';
 
-  var password = null; // use object to make password that fits criteria
-  
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+  var specialCharacters = ['!','@','#','$','%','^','&','*','(',')'];
+
+  var included = [];
+
+  if (passwordObject.fontCase === 'both') {
+    password = password + lowerCase[randomNumber(0, lowerCase.length - 1)];
+    password = password + upperCase[randomNumber(0, upperCase.length - 1)];
+    included = included.concat(upperCase);
+    included = included.concat(lowerCase);
+  }
+
+  if (passwordObject.fontCase === 'lower') {
+    password = password + lowerCase[randomNumber(0, lowerCase.length - 1)];
+    included = included.concat(lowerCase);
+  }
+
+  if (passwordObject.fontCase === 'upper') {
+    password = password + upperCase[randomNumber(0, upperCase.length - 1)];
+    included = included.concat(upperCase);
+  }
+
+  if (passwordObject.number === 'yes') {
+    password = password + numbers[randomNumber(0, numbers.length -1)];
+    included = included.concat(numbers);
+  }
+
+  if (passwordObject.specialChar === 'yes') {
+    password = password + specialCharacters[randomNumber(0, specialCharacters.length - 1)];
+    included = included.concat(specialCharacters);
+  }
+
+  // loop until we have a password that is as long as passwordObject.length
+  for(var i = password.length; i < passwordObject.length; i++) {
+    var randomCharacter = included[randomNumber(0, included.length - 1)];
+
+    password = password + randomCharacter;
+  }
+
   return password;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var password = generatePassword();
 
   passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
